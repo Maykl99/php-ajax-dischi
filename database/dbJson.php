6e28b3ccdc8a0d787df98a074517b2cd4ec97ddf
Maykl Hakobyan <maicolhakobyan853@yahoo.it>
2020-09-21 12:02:44 +0200

    db

--- database/dbJson.php
+++ database/dbJson.php
@@ -1,45 +1,14 @@
 <?php
-$database = [
-    [
-        'title' => 'New Jersey',
-        'author' => 'Bon Jovi',
-        'year' => 1988,
-        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
-    ],
-    [
-        'title' => 'Live at Wembley 86',
-        'author' => 'Queen',
-        'year' => 1992,
-        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg'
-    ],
-    [
-        'title' => 'Ten\'s Summoner\'s Tales',
-        'author' => 'Sting',
-        'year' => 1993,
-        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg'
-    ],
-    [
-        'title' => 'Steve Gadd band',
-        'author' => 'Steve Gadd Band',
-        'year' => 2018,
-        'poster' => 'https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg'
-    ],
-    [
-        'title' => 'Brave new World',
-        'author' => 'Eric Clapton', #Iron Maiden
-        'year' => 2000,
-        'poster' => 'https://i.ebayimg.com/images/g/1GwAAOSwFytaKr7l/s-l300.jpg'
-    ],
-    [
-        'title' => 'One more car, one more rider',
-        'author' => 'Eric Clapton', 
-        'year' => 2002,
-        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg'
-    ]
-];
+    include 'db.php';
+
+    // vedi se l'autore è vuoto
+    // se si fai echo json_encode($database);
+
+    // altrimenti
+    // ti calcoli il nuovo ti crei 
 
 
- #trasformazione in formato json
+    #trasformazione in formato json
     header('Content-Type: application/json');
     echo json_encode($database);
 
